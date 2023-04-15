3acaa9f4dbe80d73a6944229b78ea6194c9cd2b2
Sohaib Raja <raja.sohaib7743@gmail.com>
2023-04-15 16:26:55 -0500

    Added Tabs and Setup Forms in Manage Inventory

--- src/components/Manager/ManageInventory.vue
+++ src/components/Manager/ManageInventory.vue
@@ -1,64 +1,36 @@
 <template>
-  <nav
-    class="navbar py-3 navbar-dark navbar-expand-lg"
-    style="background-color: rgb(46, 56, 116)"
-  >
+  <nav class="navbar py-3 navbar-dark navbar-expand-lg" style="background-color: rgb(46, 56, 116)">
     <div class="container-fluid">
       <a class="navbar-brand" href="/manager">
         <img src="/src/assets/p&hlogo.png" alt="Pom & Honey Logo" width="80" />
       </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div
-        class="collapse navbar-collapse text-white"
-        id="navbarSupportedContent"
-      >
+      <div class="collapse navbar-collapse text-white" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <router-link class="nav-link" to="/manager">Dashboard</router-link>
           </li>
           <li class="nav-item">
-            <router-link
-              class="nav-link active"
-              aria-current="page"
-              to="/manager/inventory"
-              >Manage Inventory</router-link
-            >
+            <router-link class="nav-link active" aria-current="page" to="/manager/inventory">Manage
+              Inventory</router-link>
           </li>
           <li class="nav-item dropdown">
-            <router-link
-              class="nav-link dropdown-toggle"
-              to="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
+            <router-link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
               Reports
             </router-link>
             <ul class="dropdown-menu">
               <li>
-                <router-link class="dropdown-item" to="#"
-                  >Sales Report</router-link
-                >
+                <router-link class="dropdown-item" to="#">Sales Report</router-link>
               </li>
               <li>
-                <router-link class="dropdown-item" to="#"
-                  >Excess Report</router-link
-                >
+                <router-link class="dropdown-item" to="#">Excess Report</router-link>
               </li>
               <li>
-                <router-link class="dropdown-item" to="#"
-                  >X and Z Report</router-link
-                >
+                <router-link class="dropdown-item" to="#">X and Z Report</router-link>
               </li>
             </ul>
           </li>
@@ -67,9 +39,7 @@
     </div>
   </nav>
   <div class="background">
-    <div
-      class="inventory container align-items-center justify-content-center rounded bg-white"
-    >
+    <div class="inventory container align-items-center justify-content-center rounded bg-white">
       <div class="row">
         <div class="col">
           <h1 class="mb-3">Current Inventory</h1>
@@ -94,127 +64,122 @@
         </div>
       </div>
     </div>
-    <div
-      class="container align-items-center mt-4 mb-3 justify-content-center rounded bg-white"
-    >
-      <div class="row">
-        <div class="col">
-          <h1 class="mb-4">Add Item</h1>
-        </div>
-      </div>
-      <form>
-        <div class="row">
-          <div class="col">
-            <select class="form-select" aria-label="Select Category">
-              <option selected>Choose Category</option>
-              <option value="1">base</option>
-              <option value="2">side</option>
-              <option value="3">extra</option>
-            </select>
-          </div>
-          <div class="col">
-            <div class="form-group">
-              <input
-                type="text"
-                for="item-name-add"
-                class="form-control"
-                id="item-name-add"
-                placeholder="Enter Item Name"
-              />
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col"></div>
-          <div class="col mt-3">
-            <div class="form-group">
-              <input
-                type="text"
-                for="quantity-add"
-                class="form-control"
-                id="quantity-add"
-                placeholder="Enter Item Quantity"
-              />
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col"></div>
-          <div class="col mt-3">
-            <div class="form-group">
-              <input
-                type="text"
-                for="price-add"
-                class="form-control"
-                id="price-add"
-                placeholder="Enter Item Price"
-              />
-            </div>
-          </div>
-        </div>
-        <button type="submit" class="btn mt-4 btn-primary">Submit Item</button>
-      </form>
-    </div>
-    <div
-      class="container align-items-center mt-4 mb-3 justify-content-center rounded bg-white"
-    >
-      <div class="row">
-        <div class="col">
-          <h1 class="mb-4">Edit Item</h1>
-        </div>
-      </div>
-      <form>
-        <div class="row">
-          <div class="col">
-            <select class="form-select" aria-label="Select Item">
-              <option selected>Select Item</option>
-              <option value="{{ index }}" v-for="item in Inventory">{{ item.name }}</option>
-            </select>
-          </div>
-          <div class="col">
-            <select class="form-select" aria-label="Select Item">
-              <option selected>Select Attribute</option>
-              <option value="1">ID</option>
-              <option value="2">Name</option>
-              <option value="3">Category</option>
-              <option value="4">Price</option>
-            </select>
-          </div>
-          <div class="col">
-            <div class="form-group">
-              <input
-                type="text"
-                for="item-name-add"
-                class="form-control"
-                id="item-name-add"
-                placeholder="Enter Item Name"
-              />
-            </div>
-          </div>
-        </div>
-        <button type="submit" class="btn mt-4 btn-primary">Edit Item</button>
-      </form>
-    </div>
-    <div
-      class="container align-items-center mt-4 mb-3 justify-content-center rounded bg-white"
-    >
-      <div class="row">
-        <div class="col">
-          <h1 class="mb-4">Remove Item</h1>
-        </div>
-        <form>
-          <div class="row">
-            <div class="col">
-              <select class="form-select" aria-label="Select Item">
-                <option selected>Select Item</option>
-              <option v-for="item in Inventory">{{ item.name }}</option>
-              </select>
-            </div>
-          </div>
-          <button type="submit" class="btn mt-4 btn-primary">
+    <div class="container align-items-center mt-4 mb-3 justify-content-center rounded bg-white">
+      <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+          <button class="nav-link active" id="add-tab" data-bs-toggle="tab" data-bs-target="#add" type="button"
+            role="tab" aria-controls="add" aria-selected="true">
+            Add Item
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button class="nav-link" id="edit-tab" data-bs-toggle="tab" data-bs-target="#edit" type="button"
+            role="tab" aria-controls="edit" aria-selected="false">
+            Edit Item
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button class="nav-link" id="remove-tab" data-bs-toggle="tab" data-bs-target="#remove" type="button"
+            role="tab" aria-controls="remove" aria-selected="false">
             Remove Item
           </button>
-        </form>
+        </li>
+      </ul>
+      <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active mt-5" id="add" role="tabpanel" aria-labelledby="add-tab">
+          <!-- <h5>Category: {{ SelectedAddCategory }}, Name: {{ AddItemName }}, #: {{ AddItemQuantity }}, Price: {{ AddItemPrice }}</h5> -->
+          <form>
+            <div class="row">
+              <div class="col">
+                <select class="form-select" v-model="SelectedAddCategory">
+                  <option value="">Choose Category</option>
+                  <option>Base</option>
+                  <option>Protein</option>
+                  <option>Topping</option>
+                  <option>Drink</option>
+                  <option>Dressing</option>
+                  <option>Side</option>
+                  <option>Extra</option>
+                </select>
+              </div>
+              <div class="col">
+                <div class="form-group">
+                  <input v-model="AddItemName" type="text" for="item-name-add" class="form-control" id="item-name-add"
+                    placeholder="Enter Item Name" />
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"></div>
+              <div class="col mt-3">
+                <div class="form-group">
+                  <input v-model="AddItemQuantity" type="text" for="quantity-add" class="form-control" id="quantity-add"
+                    placeholder="Enter Item Quantity" />
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col"></div>
+              <div class="col mt-3">
+                <div class="form-group">
+                  <input v-model="AddItemPrice" type="text" for="price-add" class="form-control" id="price-add" placeholder="Enter Item Price" />
+                </div>
+              </div>
+            </div>
+            <button type="submit" class="btn mt-4 btn-primary">
+              Submit Item
+            </button>
+          </form>
+        </div>
+        <div class="tab-pane fade mt-5" id="edit" role="tabpanel" aria-labelledby="edit-tab">
+          <!-- <h5>Item: {{ SelectedEditItem }}, Attribute: {{ SelectedEditAttribute }}, Name: {{ EditItemName }}</h5> -->
+          <form>
+            <div class="row">
+              <div class="col">
+                <select class="form-select" v-model="SelectedEditItem">
+                  <option value="">Select Item</option>
+                  <option v-for="item in Inventory">
+                    {{ item.name }}
+                  </option>
+                </select>
+              </div>
+              <div class="col">
+                <select class="form-select" v-model="SelectedEditAttribute">
+                  <option value="">Select Attribute</option>
+                  <option>Name</option>
+                  <option>Category</option>
+                  <option>Price</option>
+                  <option>Quantity</option>
+                </select>
+              </div>
+              <div class="col">
+                <div class="form-group">
+                  <input v-model="EditItemName" type="text" for="item-name-add" class="form-control" id="item-name-add"
+                    placeholder="Enter Item Name" />
+                </div>
+              </div>
+            </div>
+            <button type="submit" class="btn mt-4 btn-primary">
+              Edit Item
+            </button>
+          </form>
+        </div>
+        <div class="tab-pane fade mt-5" id="remove" role="tabpanel" aria-labelledby="remove-tab">
+          <!-- <h5>Item: {{ SelectedRemoveItem }}</h5> -->
+          <form>
+            <div class="row">
+              <div class="col">
+                <select v-model="SelectedRemoveItem" class="form-select">
+                  <option value="">Select Item</option>
+                  <option v-for="item in Inventory">{{ item.name }}</option>
+                </select>
+              </div>
+            </div>
+            <button type="submit" class="btn mt-4 btn-primary">
+              Remove Item
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -237,6 +202,14 @@ export default {
   data() {
     return {
       Inventory: {},
+      SelectedAddCategory: '',
+      AddItemName: '',
+      AddItemQuantity: '',
+      AddItemPrice: '',
+      SelectedEditItem: '',
+      SelectedEditAttribute: '',
+      EditItemName: '',
+      SelectedRemoveItem: '',
     };
   },
   mounted() {
--- src/components/Manager/Manager.vue
+++ src/components/Manager/Manager.vue
@@ -1,66 +1,35 @@
 <template>
-  <nav
-    class="navbar py-3 navbar-dark navbar-expand-lg"
-    style="background-color: rgb(46, 56, 116)"
-  >
+  <nav class="navbar py-3 navbar-dark navbar-expand-lg" style="background-color: rgb(46, 56, 116)">
     <div class="container-fluid">
       <a class="navbar-brand" href="/manager">
         <img src="/src/assets/p&hlogo.png" alt="Pom & Honey Logo" width="80" />
       </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div
-        class="collapse navbar-collapse text-white"
-        id="navbarSupportedContent"
-      >
+      <div class="collapse navbar-collapse text-white" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link
-              class="nav-link active"
-              aria-current="page"
-              to="/manager"
-              >Dashboard</router-link
-            >
+            <router-link class="nav-link active" aria-current="page" to="/manager">Dashboard</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/manager/inventory"
-              >Manage Inventory</router-link
-            >
+            <router-link class="nav-link" to="/manager/inventory">Manage Inventory</router-link>
           </li>
           <li class="nav-item dropdown">
-            <router-link
-              class="nav-link dropdown-toggle"
-              to="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
+            <router-link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
               Reports
             </router-link>
             <ul class="dropdown-menu">
               <li>
-                <router-link class="dropdown-item" to="#"
-                  >Sales Report</router-link
-                >
+                <router-link class="dropdown-item" to="#">Sales Report</router-link>
               </li>
               <li>
-                <router-link class="dropdown-item" to="#"
-                  >Excess Report</router-link
-                >
+                <router-link class="dropdown-item" to="#">Excess Report</router-link>
               </li>
               <li>
-                <router-link class="dropdown-item" to="#"
-                  >X and Z Report</router-link
-                >
+                <router-link class="dropdown-item" to="#">X and Z Report</router-link>
               </li>
             </ul>
           </li>
@@ -71,15 +40,9 @@
   <div class="background">
     <div class="container">
       <div class="row">
-        <div
-          class="col py-4 rounded-pill container d-flex align-items-center justify-content-center bg-white"
-          style="margin-top: 50px"
-        >
-          <img
-            src="src/assets/user-icon.png"
-            alt="User's Profile Picture"
-            width="50"
-          />
+        <div class="col py-4 rounded-pill container d-flex align-items-center justify-content-center bg-white"
+          style="margin-top: 50px">
+          <img src="src/assets/user-icon.png" alt="User's Profile Picture" width="50" />
           <h1 style="margin-left: 15px">Howdy, {{ name }}!</h1>
         </div>
       </div>
@@ -88,7 +51,7 @@
 </template>
 
 <style scoped>
-.background{
+.background {
   height: 95vh;
   background-color: lightgray;
 }
