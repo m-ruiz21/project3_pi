544198a6b98eee30cc551e3292050a315f83d107
Sohaib Raja <raja.sohaib7743@gmail.com>
2023-04-26 14:37:06 -0500

    Updated Resizing of Customer Navbar

--- src/components/Customer/Navbar.vue
+++ src/components/Customer/Navbar.vue
@@ -6,25 +6,25 @@
       </a>
       <div class="justify-content-end">
         <ul class="navbar-nav">
-
           <li class="nav-item" style="margin-right: 5vw;">
-            <button class="btn-secondary" style="color: rgb(46,56,116); width: fit-content;" onclick="location.href = '/about'">About Us</button>
+            <button class="btn-secondary about" style="color: rgb(46,56,116); width: fit-content;"
+              onclick="location.href = '/about'">About Us</button>
           </li>
           <li v-if="!isAuthenticated" class="nav-item" style="margin-right: 10px;">
             <button class="btn profile" @click="login"><img class="rounded-circle" height="42"
                 src="/src/assets/user-icon.png"></button>
           </li>
-          <li v-else class="nav-item log-in dropdown">
-            <img class="rounded-circle profile" style="margin-right: 20px; margin-top: 2px;" height="42" to="#" role="button" data-bs-toggle="dropdown"
-              aria-expanded="false" :src="user.picture">
+          <li v-else class="nav-item dropdown">
+            <img class="rounded-circle profile-signin" style="margin-right: 20px; margin-top: 2px;" height="42" to="#"
+              role="button" data-bs-toggle="dropdown" aria-expanded="false" :src="user.picture">
             <ul class="dropdown-menu justify-content-center text-center">
               <li>
                 <button class="btn" @click="logout">Sign out</button>
               </li>
             </ul>
           </li>
-          <li class="nav-item">
-            <a class="profile" href="#">
+          <li class="nav-item cart-li">
+            <a class="cart" href="#">
               <img src="../../assets/cart.svg" alt="Cart" height="30">
             </a>
           </li>
@@ -41,18 +41,19 @@ h1 {
 }
 
 .btn-secondary {
-    width: 40%;
-    background-color: white;
-    color: #242B64;
-    border-radius: 20px;
-    border: 2px solid #242B64;
-    padding: 9px 22px;
-    font-size: 18px;
-    font-weight: bold;
+  width: 40%;
+  background-color: white;
+  color: #242B64;
+  border-radius: 20px;
+  border: 2px solid #242B64;
+  padding: 7px 15px;
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 2px;
 }
 
-.btn-secondary:hover{
-    background-color: rgb(248, 241, 241);
+.btn-secondary:hover {
+  background-color: rgb(218, 218, 218);
 }
 
 .profile {
@@ -60,8 +61,56 @@ h1 {
   font-size: 25px;
 }
 
+.profile-signin {
+  margin-top: -6px;
+  font-size: 25px;
+}
+
+.cart {
+  margin-top: -6px;
+  font-size: 25px;
+}
+
 .profile:hover {
-  color: rgb(212, 0, 0);
+  filter: invert(80%);
+}
+
+.profile-signin:hover {
+  filter: brightness(70%)
+}
+
+.cart:hover {
+  filter: invert(50%);
+}
+
+@media (max-width: 770px) {
+
+  .about {
+    margin-right: 120px;
+  }
+
+  .profile {
+    margin-left: 110px;
+    margin-top: -73px;
+  }
+
+  .profile-signin {
+    margin-left: 124px;
+    margin-top: -70px !important;
+  }
+
+  .container {
+    width: 100vw;
+  }
+
+  .cart {
+    margin-left: 180px !important;
+  }
+
+  .cart-li {
+    margin-top: -66px !important;
+  }
+
 }
 </style>
 
