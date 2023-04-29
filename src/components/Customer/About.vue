37d75d05573a2f1125622785236bb66eb76d8bc3
andrewcwin <andrewcwin@gmail.com>
2023-04-29 18:18:17 -0500

    styling of about page

--- src/components/Customer/About.vue
+++ src/components/Customer/About.vue
@@ -10,27 +10,37 @@
     </div> -->
   </div>
   <div class="row justify-content-center">
-  <div class="col-md-6 d-flex align-items-center">
-    <div class="about-text-box">
-      <p class="text-center mt-3 px-5">
-        We are Pom and Honey, a Mediterranean grill located in the heart of our beloved aggie campus. We are proud to be a
-        part of the local A&M community serving fresh and healthy food inspired by the flavors of the Mediterranean.
-        Our menu features a variety of dishes, including meatballs, falafel, and chipotle-style dishes.
-        At Pom and Honey, we are committed to providing our customers with exceptional service and a warm and welcoming
-        atmosphere.
-        Whether you're dining in or taking out, we strive to make every visit a memorable one.
-        Thank you for choosing Pom and Honey. We look forward to serving you soon!
-      </p>
+    <div class="col-md-6 mt-5 d-flex align-items-center justify-content-center">
+      <img src="src/assets/about2.png" class="img-fluid" style="height: 100%; width: 100%; object-fit: cover;">
     </div>
-    <!-- <div class="about-image-box">
-      <img src="src/assets/aboutus.png" class="img-fluid" style="width: 100% height: 100%;">
-    </div> -->
-  </div>
+    <div class="col-md-6 d-flex align-items-center">
+      <div class="about-text-box mt-3 px-5">
+        <h1 style="font-weight: bold;"> OUR MISSION </h1>
+        <h2 style="font-weight: semi-bold"> Fresh, healthy Mediterranean cuisine & exceptional service. </h2>
+        <p class="text-start mt-3">
+          We are Pom and Honey, a Mediterranean grill located in the heart of our beloved aggie campus. We are proud to be a
+          part of the local A&M community serving fresh and healthy food inspired by the flavors of the Mediterranean.
+          Our menu features a variety of dishes, including meatballs, falafel, and chipotle-style dishes.
+          At Pom and Honey, we are committed to providing our customers with exceptional service and a warm and welcoming
+          atmosphere.
+          Whether you're dining in or taking out, we strive to make every visit a memorable one.
+          Thank you for choosing Pom and Honey. We look forward to serving you soon!
+        </p>
+      </div>
+    </div>
 </div>
-  <h1 class="text-center">Where To Find Us</h1>
+  <h1 class="text-center mt-5" style="font-weight: bold;">OUR LOCATION</h1>
   <div class="gmap">
     <iframe width="70%" height="450" style="border:0" loading="lazy" allowfullscreen
       :src="maps_request"></iframe>
+  </div>
+
+  <div>
+
+    <div class="text-center mt-5">
+      <h2 class="mb-3">Distance from current location:</h2>
+      <p class="lead">{{ distance }}</p>
+    </div>
   </div>
 
 
@@ -51,7 +61,6 @@ img {
   width: 100%;
   height: 0%;
   border: 1px solid;
-  border-color: black;
 }
 
 image-footer{
